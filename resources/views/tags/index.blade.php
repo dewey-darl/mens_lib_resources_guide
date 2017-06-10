0a3aaa90e588b30403103fc00aa0f9fef97f5418
Andrew Pereli <drewpereli@gmail.com>
2017-06-10 15:59:09 -0500

    added new tag form to tags page

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -2,42 +2,49 @@
 @extends('layouts.app')
 
 @section('content')
-	@foreach ($tags as $tag)
-		<div class="row">
-			<h3 class='col-xs-12'>{{$tag->readableName()}}</h3>
-			<div class="col-xs-12">
-				Created by {{$tag->user['username']}} on {{date('F jS, Y', strtotime($tag->created_at))}}
-			</div>
-			<div class="col-xs-12">&nbsp;</div>
-			<div class="col-xs-6">
-				@include('templates.buttons.edit', ['text' => 'Edit', 'class' => 'edit-btn'])
-				<br/><br/>
-				<div class="">
-					{!! 
-						Form::model(
-							$tag,
-							[
-								'method' => 'put',
-								'action' => ['TagController@update', $tag],
-								'class' => 'edit-form',
-								'style' => 'display: none;'
-							]
-						) 
-					!!}
-						<div class="form-group">
-							{!! Form::text('tag_name', $tag->name, ['class' => 'form-control']) !!}
-						</div>
-						@include('templates.buttons.post', ['text' => 'Update'])
-						@include('templates.buttons.cancel', ['text' => 'Cancel', 'class' => 'cancel-btn'])
-					{!! Form::close() !!}
+	<div class="col-xs-8">
+		<h2>Edit Tags</h2>
+		@foreach ($tags as $tag)
+			<div class="row">
+				<h3 class='col-xs-12'>{{$tag->readableName()}}</h3>
+				<div class="col-xs-12">
+					Created by {{$tag->user['username']}} on {{date('F jS, Y', strtotime($tag->created_at))}}
+				</div>
+				<div class="col-xs-12">&nbsp;</div>
+				<div class="col-xs-6">
+					@include('templates.buttons.edit', ['text' => 'Edit', 'class' => 'edit-btn'])
+					<br/><br/>
+					<div class="">
+						{!! 
+							Form::model(
+								$tag,
+								[
+									'method' => 'put',
+									'action' => ['TagController@update', $tag],
+									'class' => 'edit-form',
+									'style' => 'display: none;'
+								]
+							) 
+						!!}
+							<div class="form-group">
+								{!! Form::text('tag_name', $tag->name, ['class' => 'form-control']) !!}
+							</div>
+							@include('templates.buttons.post', ['text' => 'Update'])
+							@include('templates.buttons.cancel', ['text' => 'Cancel', 'class' => 'cancel-btn'])
+						{!! Form::close() !!}
+					</div>
+				</div>
+				<div class="col-xs-6">
+					@include('templates.safe_delete_form', ['model' => $tag])
 				</div>
 			</div>
-			<div class="col-xs-6">
-				@include('templates.safe_delete_form', ['model' => $tag])
-			</div>
-		</div>
-		<hr/>
-	@endforeach
+			<hr/>
+		@endforeach
+	</div>
+	<div class="col-xs-3 col-xs-offset-7 affix">
+		<h2>Add a Tag</h2>
+		@include('templates.new_tag_form')
+	</div>
 	<script>
 		$('.edit-btn').click(function(){
 			$(this).parent().find('.edit-form').fadeIn();
