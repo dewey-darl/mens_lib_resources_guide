a350c64aaa5d80955a758e0ed1dfb38f50229146
Andrew Pereli <drewpereli@gmail.com>
2017-06-10 15:53:24 -0500

    made resources search bar toggleable

--- resources/views/resources/index.blade.php
+++ resources/views/resources/index.blade.php
@@ -12,16 +12,30 @@
 @section('content')
 
 <div class="row">
-	<div class="col-xs-12 col-md-10 col-md-offset-1">
-	{!! Form::open() !!}
-		<!-- Hidden checkbox field for tags -->
-		@include('templates.tag_selector', ['selected_tags' => $selected_tags])
-		<br/><br/>
-		{!! Form::button('Get Resources With Any Of These Tags', ['class' => 'btn btn-info search-any']) !!}
-		&nbsp;
-		{!! Form::button('Get Resources With All Of These Tags', ['class' => 'btn btn-info search-all']) !!}
+	<div class="col-xs-12 col-md-10 col-md-offset-1 panel">
+		<button type="button" class="btn btn-info btn-lg collapse-btn" data-toggle='collapse' data-target='#resource-search-form'>
+			<span class="text">
+				Search for Resources&nbsp;@include('templates.glyphicon', ['type' => 'chevron-right'])
+			</span> 
+			<span class="text" style="display:none;">
+				Hide Search Form&nbsp;@include('templates.glyphicon', ['type' => 'chevron-down'])
+			</span> 
+		</button>
+		<script>
+			$(".collapse-btn").click(function(){
+				$(this).find('.text').toggle();
+			});
+		</script>
+		{!! Form::open(['id' => 'resource-search-form', 'class' => 'collapse']) !!}
+			<div class="col-xs-12">&nbsp;</div>
+			<!-- Hidden checkbox field for tags -->
+			@include('templates.tag_selector', ['selected_tags' => $selected_tags])
+			<br/><br/>
+			{!! Form::button('Get Resources With Any Of These Tags', ['class' => 'btn btn-info search-any']) !!}
+			&nbsp;
+			{!! Form::button('Get Resources With All Of These Tags', ['class' => 'btn btn-info search-all']) !!}
 		{!! Form::close() !!}
-		</div>
+	</div>
 		<script>
 			$(".search-any, .search-all").click(function(){
 				var tagsSelected = [];
